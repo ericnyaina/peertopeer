81a1fb2de89c415bbd59f37e84000ffa2732b17f
ericnyaina <ericnyainathd@gmail.com>
2020-12-05 19:36:00 +0300

    creation de la parallelisation presque ok cote serveur etape: ecoute serveur ok

--- mod.py
+++ mod.py
@@ -1,7 +1,25 @@
 import socket as sk
+import sys
+from threading import Thread
+import time
 
-conn = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
+id_port = 12800
 
-conn.bind(('', 12800))
-conn.listen(5)
-conn_client, infos_conn = conn.accept()
+class serveur(Thread):
+
+    def __init__(self):
+		Thread.__init__(self)
+		self.conn = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
+		self.conn.bind(('', id_port))
+		self.conn.listen(5)
+
+    def run(self):
+        
+		while 1:
+			conn_client, infos_conn = self.conn.accept()
+			print "on recoit un nouveau client : " + str(infos_conn)
+			
+srv = serveur()
+srv.start()
+
+
